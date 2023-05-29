bd1dcfaf2829562e1dbab14a24fef0ff118f802c
mireiavh <mirvazher02@gmail.com>
2023-05-29 20:10:51 +0200

    v1.1 piano + navbar

--- piano/index.js
+++ piano/index.js
@@ -1,3 +1,14 @@
+/* import { goToBttn } from "../scripts/script.js";
+
+document.getElementById("homeDrumBttn").addEventListener("click", (event) => {
+    goToBttn("./../tambor/drum.html");
+});
+
+
+document.getElementById("homePianoBttn").addEventListener("click", (event) => {
+    goToBttn("./../piano/index.html");
+});
+*/
 
 const $allKeys = document.querySelectorAll(".key")
 $allKeys.forEach(key => 
