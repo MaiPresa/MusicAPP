5f6f9551447724f0d45d09361b24ddfd3171141b
mireiavh <mirvazher02@gmail.com>
2023-06-02 11:59:59 +0200

    Revert "Esta es una prueba. No usar" Fixed This reverts commit 844305927f6436d7bf4040d69811f04736cd86b5.

--- piano/index.js
+++ piano/index.js
@@ -28,12 +28,26 @@ const keyboardNotes = {
   "h": "G1",
   "j": "A1",
   "k": "B1",
+                /* Teclas Adicionales */
+  //"z": "C2",
+  //"x": "D2",
+  //"c": "E2",
+  //"v": "F2",
+  //"b": "G2",
+  //"n": "A2",
+  //"m": "B2",
 
   "w": "C1s",
   "e": "D1s",
   "y": "F1s",
   "u": "G1s",
   "i": "A1s",
+                /* Teclas Adicionales */
+  //"s": "C2s",
+  //"d": "D2s",
+  //"g": "F2s",
+  //"h": "G2s",
+  //"j": "A2s",
 }
 document.addEventListener("keydown", (event) => {
   if (event.repeat) return  
@@ -41,9 +55,11 @@ document.addEventListener("keydown", (event) => {
   const notePressed = keyboardNotes[event.key.toLowerCase()]
 
   if (notePressed) {
+    // Mudar cor background
     const $noteElement = document.querySelector(`[data-music-note="${notePressed}"]`)
     $noteElement.classList.add("clicked")
 
+    // Tocar audio
     const audio = new Audio(`./piano-keys/${notePressed}.wav`)
     audio.play()
   }
