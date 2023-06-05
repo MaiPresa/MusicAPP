ca4943a7290ff9178e8cb3d85ae6087364f457c4
Rocio <rocioalonso.dev@gmail.com>
2023-06-05 11:18:17 +0200

    v1.8 // clean code

--- menu/js/navigation.js
+++ menu/js/navigation.js
@@ -3,27 +3,26 @@ let header = document.querySelector('header');
 
 navigation = {
     build: function(){
-        //creo el link y logo, y lo inserto
+
         let logo = document.createElement('img');
         logo.src = '../../menu/img/logo.svg';
         logo.setAttribute("id", "logo");
         header.appendChild(logo);
 
-        //creo primer div decorativo e inserto
+
         let firstDecorativeNavBox = document.createElement('div');
         header.appendChild(firstDecorativeNavBox);
 
-        //creo el nav y lo inserto
+
         let nav = document.createElement('nav');
         nav.setAttribute('id','navigation');
         header.appendChild(nav);
 
-        //creo la lista y la inserto en el nav
+
         let navList = document.createElement('ul');
         nav.appendChild(navList);
 
-        
-        //bucle para crear 3 elementos en la lista
+
         for(let i=0;i<3;i++){
             let navListItem = document.createElement('li');
             navList.appendChild(navListItem);
@@ -34,33 +33,23 @@ navigation = {
             }
         }
 
-        //creo div decorativo e inserto
+
         let secondDecorativeNavBox = document.createElement('div');
         header.appendChild(secondDecorativeNavBox);
 
-        //creo otro div decorativo e inserto
+
         let thirdDecorativeNavBox = document.createElement('div');
         header.appendChild(thirdDecorativeNavBox);
 
     },
-    active:function(){
-        
-    },
-    hover: function(){
-        pianoButton
-    },
 };
 
-//creamos la estructura de la página
+
 navigation.build();
-navigation.active();
 
 
-//elementos con llamada a la acción
 let pianoButton = document.getElementById("navButtonPiano");
 let drumsButton = document.getElementById("navButtonDrums");
-// let userButton = document.getElementById("navButtonUser");
-
 
 
 drumsButton.addEventListener("click", (event) => {
